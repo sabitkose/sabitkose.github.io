4cfc751bf9fe3da0ff435ed1084fe3b72b6e0342
sabitkose <sabit.kose@gmail.com>
2022-01-19 13:29:40 +0300

    added sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,7 @@ PLUGINS = ['sitemap']
 
 SITEMAP = {
     "format": "xml",
-    "exclude": ["tag/", "category/", "author/", ],
+    "exclude": ["tag/", "category/", "author/", "tags.html", "categories.html", "authors.html", "archives.html" ],
     "priorities": {
         "articles": 0.5,
         "indexes": 0.5,
